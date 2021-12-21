d5896dfda9645ffffb30d92bfcf2ede51d6bd6bc
abhishek jha <p.singhwelcome@gmail.com>
2021-12-21 13:21:21 +0530

    msg

---
+++ U I/J S/scripts.js
@@ -0,0 +1,17 @@
+function aboutMe(){
+
+	document.write("<h1> Pushpendra Singh Kushwah </h1>");
+
+}
+
+function ChangeColor(){
+
+	document.getElementById('demo').style.color="Green";
+	document.getElementById('demo').style.fontSize = 40px;
+}
+
+function getColor(){
+
+	document.getElementById('demo').style.color="Yellow";
+	
+}
